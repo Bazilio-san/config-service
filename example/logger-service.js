9ff33e4a4fc1ab3e176a305cdc40515a69216658
vvmakarov <vvmakarov@corp.finam.ru>
2022-01-05 03:29:03 +0300

    af-logger 2.0.14

--- example/logger-service.js
+++ example/logger-service.js
@@ -1,20 +1,16 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
-const Logger = require('af-logger');
-
-const loggerOptions = {
-  prefix: '',
-  errorPrefix: 'error-',
-  suffix: 'config-service',
-  removeEmptyErrorFiles: true,
-  removeEmptyLogFiles: true
-};
+const { getAFLogger } = require('af-logger');
 
 let globalLogger;
 
 function getLogger () {
   if (!globalLogger) {
-    globalLogger = new Logger(loggerOptions);
+    const { logger } = getAFLogger({
+      minLevel: 'silly',
+      name: 'config-service'
+    });
+    globalLogger = logger;
   }
   return globalLogger;
 }
