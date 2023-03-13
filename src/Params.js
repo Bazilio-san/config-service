3137161d204f8943950db8abcd346fd1ee5256b6
vvmakarov <vvmakarov@corp.finam.ru>
2023-03-13 21:36:21 +0300

    2023-03-13--1

--- src/Params.js
+++ src/Params.js
@@ -197,7 +197,7 @@ module.exports = class Params extends Schema {
     }
     let jsonStr;
     try {
-      jsonStr = JSON.stringify(configValue, undefined, this.jsonStringifySpace);
+      jsonStr = this.jsonStringifyCompact(configValue, this.jsonStringifySpace);
     } catch (err) {
       throw this._error(`Could not save named configuration «${
         this._expectedConfigDir}/${configName}.json»`, err); // Not covered with tests
