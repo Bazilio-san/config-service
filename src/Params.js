b7abfe7b46036f28dd475e849a279c33fd1fad36
vvmakarov <vvmakarov@corp.finam.ru>
2025-06-29 20:02:36 +0300

    4.1.6

--- src/Params.js
+++ src/Params.js
@@ -12,6 +12,7 @@ const _isProp_ = Symbol.for('_isProp_');
 const _onChange_ = Symbol.for('_onChange_');
 const _callerId_ = Symbol.for('_callerId_');
 const _updatedBy_ = Symbol.for('_updatedBy_');
+const _session_ = Symbol.for('_session_');
 
 /**
  * Initialization:
@@ -178,6 +179,7 @@ module.exports = class Params extends Schema {
     }
     schemaItem[_callerId_] = options.callerId;
     schemaItem[_updatedBy_] = options.updatedBy;
+    schemaItem[_session_] = options.session;
     this._traverseSchema(schemaItem, traverseOptions, this.__addNewValueCallback);
     if (absentPaths.size) {
       // console.log(`Missed:\n${[...absentPaths].join('\n')}`);
